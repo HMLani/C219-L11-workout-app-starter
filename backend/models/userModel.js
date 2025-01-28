f5846df2a4b77c9fae2df4cbbb5f0c131be851a8
HMLani <23042538@myrp.edu.sg>
2025-01-28 13:56:36 +0800

    feat: add user signup functionality with validation and JWT token generation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
@@ -13,5 +15,32 @@ const userSchema = new Schema({
         required: true
     },
 })
+
+//Static method to signup
+userSchema.statics.signup = async function( email, password ) {
+    //Validattion
+    if ( !email || !password ) {
+        throw Error('Email and password are required')
+    }
+
+    if ( !validator.isEmail( email ) ) {
+        throw Error('Invalid email')
+    }
+
+    if ( !validator.isStrongPassword( password ) ) {
+        throw Error('Password is not strong enough')
+    }
+
+    const exists = await this.findOne({ email })
+    const saltPassword = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, saltPassword)
+    const user = await this.create({ email, password: hashedPassword })
+
+    if ( exists ) {
+        throw Error('Email already in use')
+    }
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
