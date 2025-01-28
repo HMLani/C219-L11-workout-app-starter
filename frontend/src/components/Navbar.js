fee6e9b7da18205f7261f6f8b11cf3ce2d9c2f58
HMLani <23042538@myrp.edu.sg>
2025-01-28 15:40:16 +0800

    feat: add logout functionality with custom hook and button in Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,11 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 const Navbar = () => {
+  const { logout } = useLogout()
+  const handleClick = () => {
+    logout()
+  }
 
   return (
     <header>
@@ -10,6 +15,10 @@ const Navbar = () => {
         </Link>
 
         <nav>
+          <div>
+            <button onClick={handleClick}>Logout</button>
+          </div>
+
           <div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
