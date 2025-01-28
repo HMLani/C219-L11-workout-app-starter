6b34f2214f2348f3c2f5c4b5b43955419ab8cf4c
HMLani <23042538@myrp.edu.sg>
2025-01-28 15:32:39 +0800

    feat: enhance signup functionality with error handling and loading state

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,42 +4,35 @@ const validator = require('validator')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-
-    password: {
-        type: String,
-        required: true
-    },
+    email: { type: String, required: true, unique: true },
+    password: { type: String,  required: true },
 })
 
 //Static method to signup
-userSchema.statics.signup = async function( email, password ) {
-    //Validattion
-    if ( !email || !password ) {
-        throw Error('Email and password are required')
-    }
+userSchema.statics.signup = async function(email, password) {
 
-    if ( !validator.isEmail( email ) ) {
-        throw Error('Invalid email')
+    // validation
+    if (!email || !password) {
+      throw Error('All fields must be filled')
     }
-
-    if ( !validator.isStrongPassword( password ) ) {
-        throw Error('Password is not strong enough')
+    if (!validator.isEmail(email)) {
+      throw Error('Email not valid')
     }
-
+    if (!validator.isStrongPassword(password)) {
+      throw Error('Password not strong enough')
+    }
+  
     const exists = await this.findOne({ email })
-    const saltPassword = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, saltPassword)
-    const user = await this.create({ email, password: hashedPassword })
-
-    if ( exists ) {
-        throw Error('Email already in use')
+  
+    if (exists) {
+      throw Error('Email already in use')
     }
-
+  
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+  
+    const user = await this.create({ email, password: hash })
+  
     return user
 }
 
