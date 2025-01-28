cb08f6143b987712e8e37dbf19c2189a710da495
HMLani <23042538@myrp.edu.sg>
2025-01-28 14:28:33 +0800

    feat: add authentication context and provider for user management

---
+++ frontend/src/hooks/useAuthContext.js
@@ -0,0 +1,12 @@
+import { AuthContext } from '../context/AuthContextContext'
+import { useContext } from 'react'
+
+export const useWorkoutsContext = () => {
+  const context = useContext(AuthContext)
+
+  if (!context) {
+    throw Error('useAuthContext must be used inside an AuthContextProvider')
+  }
+
+  return context
+}
